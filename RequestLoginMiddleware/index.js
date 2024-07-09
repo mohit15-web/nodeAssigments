3bf2a0b5c2b85d2dfb5018027a9bf6897928b9c8
MohitChaudhari <mohitchoudharu2@gmail.com>
2024-07-09 22:18:29 +0530

    done

--- RequestLoginMiddleware/index.js
+++ RequestLoginMiddleware/index.js
@@ -1,15 +1,29 @@
-const express = require('express');
+const express=require('express')
+const  responseTime = require('response-time')
+const morgan=require('morgan')
+const requestIp = require('request-ip');
 const app = express();
+const logger = (req, res, next) => {
+  console.log("Name : Mohit");
+  console.log("method:", req.method);
+  console.log("url:", req.url);
+  // console.log("url:", requestIp);
+  console.log("timestamp:", Date.now());
+  console.log("response time:", req.responseTime);
+  console.log("ip address =", req.socket.localAddress);
+  console.log("URL=", req.url);
 
-app.get('/user', (req, res) => {
-    console.log("method:", req.method);
-    console.log("url:", req.url);
-    console.log("timestamp:", Date.now());
-    console.log("response time:", req.responseTime);
-    res.json({
-        success: true,
-        message:'request success'
-    })
+  next();
+};
+
+app.use(responseTime());
+app.use(morgan("dev"));
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "request success",
+  });
 });
-
-app.listen(8000 , () => console.log('Listening on port 8000'))
+app.listen(9000, () => console.log("Listening on port 8000"));
